7ac7a128edfbdb3da23a145697c054dc9fe68ea1
jadonpadon <salvador.jaden@gmail.com>
2024-11-22 03:08:41 -0500

    Week 10 Stuff

--- Salvador_Platformer/Assets/Scripts/PlayerController.cs
+++ Salvador_Platformer/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private new Collider2D collider;
     public float speed;
 
     public enum FacingDirection
@@ -12,10 +13,14 @@ public class PlayerController : MonoBehaviour
         left, right
     }
 
+    private FacingDirection lastDirection;
+    public LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -25,6 +30,8 @@ public class PlayerController : MonoBehaviour
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
         MovementUpdate(playerInput);
+
+        
     }
 
     private void MovementUpdate(Vector2 playerInput)
@@ -32,26 +39,53 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             playerInput = Vector2.left;
+            lastDirection = FacingDirection.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
             playerInput = Vector2.right;
+            lastDirection = FacingDirection.right;
         }
 
-        rb.velocity += playerInput * speed * Time.deltaTime;
+        rb.velocity = new Vector2(playerInput.x * speed, rb.velocity.y);
     }
 
     public bool IsWalking()
     {
-        return false;
+        if(Mathf.Abs(rb.velocity.x) > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     public bool IsGrounded()
     {
-        return true;
+        RaycastHit2D hit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
+        
+        if (hit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if(rb.velocity.x > 0)
+        {
+            return FacingDirection.right;
+        }
+        else if(rb.velocity.x < 0) 
+        { 
+            return FacingDirection.left; 
+        }
+        return lastDirection;
     }
 }
